c7cb07a4658d8e45549023ac4192025ae3e7bbc6
niten2 <niten2@yandex.ru>
2021-04-17 09:05:22 +0700

    add comment

--- private/config.el
+++ private/config.el
@@ -1,5 +1,7 @@
 (setq doom-font "Source Code Pro Semibold-9:pixelsize=26")
 
+;; (add-hook 'doom-init-ui-hook #'spacemacs/home)
+
 (custom-set-variables
  '(bmkp-last-as-first-bookmark-file
    "/home/q/code/github/dotfiles/configs/user/emacs.d/.local/etc/bookmarks")
